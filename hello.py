22d3e8d17d6e47dd8396c792432ec51303eaa547
yukito ohira <yukito.sys.so@gmail.com>
2018-01-24 22:45:39 +0900

    practice

--- hello.py
+++ hello.py
@@ -1,1 +1,21 @@
-print("Hello")
+spam = 'ham'
+
+
+def egg():
+    global spam
+    spam = 'egg'
+
+    print(spam)
+
+
+egg()
+
+try:
+    10 / 0
+except ZeroDivisionError as e:
+    print(type(e))
+finally:
+    print('block end')
+
+with open('test.txt','a') as f:
+    f.write('This is python learning')
