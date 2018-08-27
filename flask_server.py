8b2348be8e80818920ba824bda073c3fb3cff5b9
yukito ohira <yukito.sys.so@gmail.com>
2018-08-27 22:51:42 +0900

    各種基本的なルーティングを追加

--- flask_server.py
+++ flask_server.py
@@ -2,7 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    return 'Hello,World'
+    return 'Hello,Flask!'
+
+@app.route('/hello')
+def hello():
+    return 'Hello,World!'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %f' % post_id
+
+@app.route('/path/<path:subpath>')
+def sgow_subpath(subpath):
+    return 'Subpath %s' % subpath
