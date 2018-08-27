33bc63a796fbc6d5d731334b13e8fb251856d64b
yukito ohira <yukito.sys.so@gmail.com>
2018-08-27 23:27:57 +0900

    テンプレートの描画ができるようになった

--- flask/flask_server.py
+++ flask/flask_server.py
@@ -1,22 +1,28 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Hello,Flask!'
+    return 'Hello,World!'
+
 
 @app.route('/hello')
-def hello():
-    return 'Hello,World!'
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
 
 @app.route('/user/<username>')
 def show_user_profile(username):
     return 'User %s' % username
 
+
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     return 'Post %d' % post_id
+
 
 @app.route('/path/<path:subpath>')
 def sgow_subpath(subpath):
